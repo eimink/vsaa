9e3a6d42a25dfde382bfa025cf25802db3ff8ca5
Teppo Kauppinen <eimink@gmail.com>
2014-07-14 23:37:05 +0300

    Initial version
    Initial version of Unity plugin. Basically just a tester that the
    communication with server works.

---
+++ Unity/Assets/Scripts/Communicator.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+namespace vsaa
+{
+
+	public enum messageType
+	{
+		SERVER_AUTH = -100,
+		APPLICATION_EVENT_START = 0,
+		APPLICATION_EVENT_QUIT,
+		APPLICATION_EVENT_PAUSED,
+		APPLICATION_EVENT_GENERIC
+	}
+
+	public class Communicator : MonoBehaviour {
+
+		public string serverUrl;
+		public string apiKey;
+		public string apiSecret;
+		public string platformStatus;
+
+		private string appToken;
+
+		void Start () {
+			Authenticate(); // Automatically tries to authenticate with server when the script is run.
+		}
+
+
+		void OnApplicationQuit()
+		{
+			StartCoroutine(SendRequest(messageType.APPLICATION_EVENT_QUIT,"",delegate(string retval){
+				if (Application.isEditor)
+					Debug.Log(retval);
+				platformStatus = "OK!";
+			}));
+		}
+
+		void OnApplicationPaused()
+		{
+			StartCoroutine(SendRequest(messageType.APPLICATION_EVENT_PAUSED,"",delegate(string retval){
+				if (Application.isEditor)
+					Debug.Log(retval);
+				platformStatus = "OK!";
+			}));
+		}
+
+		public void SendGenericEvent(string content)
+		{
+			StartCoroutine(SendRequest(messageType.APPLICATION_EVENT_GENERIC,content,delegate(string retval){
+				if (Application.isEditor)
+					Debug.Log(retval);
+				platformStatus = "OK!";
+			}));
+		}
+
+		private void Authenticate()
+		{
+			StartCoroutine(SendRequest(messageType.SERVER_AUTH,"",delegate(string rval){
+				if (Application.isEditor)
+					Debug.Log(rval);
+				var dict = Json.Deserialize(rval) as Dictionary<string,object>;
+				appToken = (string)dict["access_token"];
+				StartCoroutine(SendRequest(messageType.APPLICATION_EVENT_START,"",delegate(string retval){
+				if (Application.isEditor)
+					Debug.Log(retval);
+					platformStatus = "OK!";
+				}));
+			}));
+		}
+
+		private string CreateAuthString()
+		{
+			return System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(apiKey+":"+apiSecret));
+		}
+
+		IEnumerator SendRequest(messageType type, string content, Action<string> onRequestCompleted)
+		{
+			string url = serverUrl;
+			WWW www;
+			WWWForm form = new WWWForm();
+			var headers = new Hashtable();
+			switch (type)
+			{
+			case messageType.SERVER_AUTH:
+				url += "login";
+				form.AddField("grant_type","client_credentials");
+				headers.Add("Authorization", "Basic "+CreateAuthString());
+				www = new WWW(url, form.data, headers);
+				break;
+			case messageType.APPLICATION_EVENT_START:
+				url += "event";
+				headers.Add("Authorization", "Bearer "+appToken);
+				form.AddField("Description","Application start");
+				form.AddField("ApiKey",apiKey);
+				www = new WWW(url, form.data, headers);
+				break;
+			case messageType.APPLICATION_EVENT_QUIT:
+				url += "event";
+				headers.Add("Authorization", "Bearer "+appToken);
+				form.AddField("Description","Application quit");
+				form.AddField("ApiKey",apiKey);
+				www = new WWW(url, form.data, headers);
+				break;
+			case messageType.APPLICATION_EVENT_PAUSED:
+				url += "event";
+				headers.Add("Authorization", "Bearer "+appToken);
+				form.AddField("Description","Application paused");
+				form.AddField("ApiKey",apiKey);
+				www = new WWW(url, form.data, headers);
+				break;
+			case messageType.APPLICATION_EVENT_GENERIC:
+				url += "event";
+				headers.Add("Authorization", "Bearer "+appToken);
+				form.AddField("Description",content);
+				form.AddField("ApiKey",apiKey);
+				www = new WWW(url, form.data, headers);
+				break;
+			default:
+				url += "token";
+				form.AddField("grant_type","client_credentials");
+				headers.Add("Authorization", "Basic "+CreateAuthString());
+				www = new WWW(url, form.data, headers);
+				break;
+			}
+			yield return www;
+			if (string.IsNullOrEmpty(www.error))
+				onRequestCompleted(www.text);
+			else
+			{
+				Debug.Log(www.error);
+				platformStatus = www.error;
+			}
+		}
+	}
+
+}
---
+++ Unity/Assets/Scripts/VerySimpleTester.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (vsaa.Communicator))]
+public class VerySimpleTester : MonoBehaviour {
+
+	vsaa.Communicator Vsaa;
+
+	string eventDescription = "Generic event";
+	
+	void Start () {
+		if (Vsaa == null)
+			Vsaa = this.GetComponent<vsaa.Communicator>();
+	}
+	
+	void OnGUI() {
+		eventDescription = GUILayout.TextField(eventDescription, GUILayout.Width(300));
+		if(GUILayout.Button("Send", GUILayout.Width(300)))
+		{
+			Vsaa.SendGenericEvent(eventDescription);
+		}
+		GUILayout.Label(Vsaa.platformStatus,GUILayout.Width(300));
+	}
+}
